494a97aac0ff4701307cefe0ed2597eec43a57b7
Farhat Ganiev <153320105+Klunz-Dev@users.noreply.github.com>
2026-02-13 22:20:43 +0300

    13.06.26
    Configuring JWT, configuring the database, and creating password hashing and verification functions

---
+++ src/server.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI, HTTPException
+
+app = FastAPI(title='SimpleAuth')
+
+@app.get('/', tags=['system'], summary='base information')
+async def base_endpoint():
+    return 'Welcome to SimpleAuth'
+
+@app.post('/create_account', tags=['user'], summary='User create new account')
+async def create_account():
+    ...
+
+@app.get('/get_account/{user_id}', tags=['user'], summary='Get user account (user_id)')
+async def get_account(user_id: int):
+    ...
+
+@app.put('/update_account/{user_id}', tags=['user'], summary='Update data account (user_id)')
+async def update_account(user_id: int):
+    ...
+
+@app.put('/delete_account/{user_id}', tags=['user'], summary='Delete account (user_id)')
+async def delete_account():
+    ...

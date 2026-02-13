f545cbde513d00cf0ad12b16dcc27d15cf01e7b6
Farhat Ganiev <153320105+Klunz-Dev@users.noreply.github.com>
2026-02-13 21:08:18 +0300

    13.02.26
    JWT configuration, database configuration, password hashing and verification functions

---
+++ SimpleAuth/src/app.py
@@ -0,0 +1,10 @@
+from colorama import *
+from art import tprint
+from hashlib import *
+import json
+import cryptography
+
+tprint(text='CryptoCloud')
+print('-'*100)
+terminal = input('>')
+print('-'*100)
